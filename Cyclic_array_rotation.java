0e9c1fd33432d36f03d8aed7af3e6d1d5e927541
Manisha <76945004+MannyP31@users.noreply.github.com>
2021-08-18 11:51:19 +0530

    geeks for geeks

--- Cyclic_array_rotation.java
+++ Cyclic_array_rotation.java
@@ -29,3 +29,39 @@ public class Test
 }
 
 
+METHOD 2 :
+
+
+import java.util.Arrays;
+
+public class Test
+{
+	static int arr[] = new int[]{1, 2, 3, 4, 5};
+	
+	
+	static void rotate()
+	{
+	int i = 0, j = arr.length - 1;
+	while(i != j)
+	{
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		i++;
+	}
+	}
+	
+	/* Driver program */
+	public static void main(String[] args)
+	{
+		System.out.println("Given Array is");
+		System.out.println(Arrays.toString(arr));
+		
+		rotate();
+		
+		System.out.println("Rotated Array is");
+		System.out.println(Arrays.toString(arr));
+	}
+}
+
+
